f7ab012d82a45bc7e8408df460164340b0fe82d6
Ellos <elias.hussien@live.se>
2024-04-22 18:47:28 +0200

    Fixed the comments to be more clear

--- src/main/java/EmailGenerator.java
+++ src/main/java/EmailGenerator.java
@@ -2,11 +2,13 @@ import java.util.Random;
 import java.util.Random;
 public class EmailGenerator {
     public static String generateRandomEmail() {
+        // Emailname + generated number from random method, and then add the domain name to it, and returns
+        // the finaly of the email adress.
         Random random = new Random();
         int uniqueNumb = random.nextInt(1001);
-        String emailName = "Elias_" + uniqueNumb; // The emailName + random generated number.
-        String domainName = "NBAplayer.com"; // The Domain for emailName
-        return emailName + "@" + domainName; // Retuns the final the email address
+        String emailName = "Elias_" + uniqueNumb;
+        String domainName = "NBAplayer.com";
+        return emailName + "@" + domainName;
     }
 }
 
